c8ac4352610004f9a2723a780dc828126dd6984d
ksdumont <ksdumont@gmail.com>
2020-02-22 16:03:23 -0800

    added file

---
+++ anagrams.js
@@ -0,0 +1,9 @@
+/* Write a function that creates an anagram list, 
+listing all the rearrangements of a given word. 
+For example, if the user types "east", the program should list all 24 permutations, 
+including "eats", "etas", "teas", and non-words like "tsae". */
+
+function anagrams(str) {
+    
+}
+console.log(anagrams('east'))
