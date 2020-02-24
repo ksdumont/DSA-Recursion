df018976b56ac1dc19ccf0b66ba74ac0fae83bcd
ksdumont <ksdumont@gmail.com>
2020-02-24 15:50:26 -0800

    finished problems

--- anagrams.js
+++ anagrams.js
@@ -3,7 +3,16 @@ listing all the rearrangements of a given word.
 For example, if the user types "east", the program should list all 24 permutations, 
 including "eats", "etas", "teas", and non-words like "tsae". */
 
-function anagrams(str) {
-    
+function anagrams(prefix, str) {
+  if (str.length <= 1) {
+    console.log(`The anagram is ${prefix}${str}`);
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      let currentLetter = str.substring(i, i + 1);
+      let previousLetters = str.substring(0, i);
+      let afterLetters = str.substring(i + 1);
+      anagrams(prefix + currentLetter, previousLetters + afterLetters);
+    }
+  }
 }
-console.log(anagrams('east'))
+console.log(anagrams('', 'word'))
--- binaryRepresentation.js
+++ binaryRepresentation.js
@@ -3,6 +3,13 @@ For example, the program should take 3 as an input and print 11 as output,
 or 25 as an input and print 11001 as an output. 
 Note that the binary representation of 0 should be 0. */
 
-function binaryRepresentation() {
-    
+function convertToBinary(num) {
+  if (num > 0) {
+    let binary = Math.floor(num % 2);
+    return convertToBinary(Math.floor(num / 2)) + binary;
+  } else {
+    return "";
+  }
 }
+
+console.log(convertToBinary(25))
